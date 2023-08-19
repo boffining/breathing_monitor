dba3f537ce6726d63e7a9ae4e7ea11b9dc519286
Nathan <nathan@Nathans-MBP.lan>
2023-08-19 16:22:36 -0400

    dumb stuff

--- main.py
+++ main.py
@@ -26,12 +26,12 @@ slice_time_end = sample_time
 
 if __name__ == '__main__':
 
-    # sleep(5)
-    # my_xep1 = Xep_data(device_name, FPS, iterations, pulses_per_step, dac_min, dac_max, area_start, area_end)
-    # my_xep1.display_sys_info()
-    # a,b = my_xep1.get_data_matrix(sample_time,save = True)
-    # my_xep1.plot_frame(a, b, sample_time)
-    # print(a.shape)
+    sleep(5)
+    my_xep1 = Xep_data(device_name, FPS, iterations, pulses_per_step, dac_min, dac_max, area_start, area_end)
+    my_xep1.display_sys_info()
+    a,b = my_xep1.get_data_matrix(sample_time,save = True)
+    my_xep1.plot_frame(a, b, sample_time)
+    print(a.shape)
 
 #     obj_amp_datamatrix = processing.slice_datamatrix(obj_amp_datamatrix, slice_time_start, slice_time_end, FPS)
 #     env_amp_datamatrix = processing.slice_datamatrix(env_amp_datamatrix, slice_time_start, slice_time_end, FPS)
